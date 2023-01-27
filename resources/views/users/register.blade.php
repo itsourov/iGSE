24bba91846986cc1d6ddce2db5d145260051f490
itsourov <sourovbuzz@gmail.com>
2023-01-27 12:30:21 +0600

    nn

--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -132,7 +132,7 @@
                         </div>
                         <div class="flex items-start">
                             <div class="flex items-center h-5">
-                                <input id="terms" aria-describedby="terms" type="checkbox"
+                                <input id="terms" aria-describedby="terms" type="checkbox" required
                                     class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800">
                             </div>
                             <div class="ml-3 text-sm">
